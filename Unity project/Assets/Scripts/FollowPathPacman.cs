aa2ff5a9562c2126dcb09839ad0c75f4f5b277a8
StefVaerten <stef.vaerten@gmail.com>
2023-02-13 10:22:59 +0100

    fixed files to open again on my pc

--- Unity project/Assets/Scripts/FollowPathPacman.cs
+++ Unity project/Assets/Scripts/FollowPathPacman.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FollowPathPacman : MonoBehaviour
 {
+    public Transform target;
+    NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        navMeshAgent.SetDestination(target.position);
     }
 }
